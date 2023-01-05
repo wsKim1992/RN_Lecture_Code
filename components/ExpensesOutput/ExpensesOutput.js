e906fa7486696054a3815ee84617fcc61354ffed
wsKim1992 <dntjr9214@gmail.com>
2023-01-06 00:57:42 +0900

    [chapter8] 133강 까지 완료

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,76 @@
+import { View, StyleSheet } from "react-native";
+import ExpensesList from "./ExpenseList";
+import ExpensesSummary from "./ExpensesSummary";
+import { GlobalStyles } from "../../constant/styles";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e12",
+    description: "A pair of shoes",
+    amount: 29.99,
+    date: new Date("2021-01-05"),
+  },
+  {
+    id: "e3",
+    description: "banana",
+    amount: 5.99,
+    date: new Date("2021-03-12"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 13.99,
+    date: new Date("2021-11-12"),
+  },
+  {
+    id: "e5",
+    description: "sushi",
+    amount: 54.01,
+    date: new Date("2021-10-05"),
+  },
+  {
+    id: "e6",
+    description: "kim bob",
+    amount: 104.01,
+    date: new Date("2021-10-05"),
+  },
+  {
+    id: "e7",
+    description: "porn",
+    amount: 154.31,
+    date: new Date("2021-11-05"),
+  },
+  {
+    id: "e8",
+    description: "gay porn",
+    amount: 154.31,
+    date: new Date("2023-01-05"),
+  },
+];
+
+const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+};
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const ManageExpenses = () => {
+  return (
+    <View>
+      <Text>Manage expenses Screen</Text>
+    </View>
+  );
+};
+
+export default ManageExpenses;
